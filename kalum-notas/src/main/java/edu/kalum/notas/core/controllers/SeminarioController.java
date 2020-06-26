5412d1572fc12afb128225903476cb6b45db9ae8
Ashly2015 <ranbr200015@gmail.com>
2020-06-26 00:33:33 -0600

    Listar seminarios

---
+++ kalum-notas/src/main/java/edu/kalum/notas/core/controllers/SeminarioController.java
@@ -0,0 +1,58 @@
+package edu.kalum.notas.core.controllers;
+
+
+import edu.kalum.notas.core.models.entity.Seminario;
+import edu.kalum.notas.core.models.service.ISeminarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.CannotCreateTransactionException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/kalum-notas/v1")
+public class SeminarioController {
+    private Logger logger= LoggerFactory.getLogger(SeminarioController.class);
+    @Autowired
+    private ISeminarioService seminarioService;
+
+    @GetMapping("/seminarios")
+    public ResponseEntity<?> listarSeminarios(){
+        logger.info("Iniciando proceso de consultas de seminarios");
+        Map<String,Object> response = new HashMap<>();
+        try {
+            logger.debug("Iniciando consulta a la base de datos");
+            List<Seminario> seminarios=this.seminarioService.findAll();
+            if (seminarios.size() == 0) {
+                logger.warn("No existen registros en la base de datos");
+                response.put("mensaje", "No exsisten registros");
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NO_CONTENT);
+            } else {
+                logger.info("Finalizando proceso de consulta de seminarios");
+                return new ResponseEntity<List<Seminario>>(seminarios,HttpStatus.OK);
+            }
+        }catch (CannotCreateTransactionException e){
+            logger.error("Error al momento de conectarse a la base de datos");
+            response.put("mensaje","Error al realizar la consulta a la base de datos");
+            response.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response,HttpStatus.SERVICE_UNAVAILABLE);
+        }
+        catch(DataAccessException e){
+            logger.error("Error al consultar la informacion a la base de datos");
+            response.put("mensaje","Error al realizar la consulta a la base de datos");
+            response.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response,HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+
+
+}
