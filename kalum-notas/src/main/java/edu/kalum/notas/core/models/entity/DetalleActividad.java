d43c5618922576a3a554b4e28cebf60012970d74
Ashly2015 <ranbr200015@gmail.com>
2020-06-26 21:10:19 -0600

    Entity detalle actividad

---
+++ kalum-notas/src/main/java/edu/kalum/notas/core/models/entity/DetalleActividad.java
@@ -0,0 +1,40 @@
+package edu.kalum.notas.core.models.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="detalle_actividad")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetalleActividad implements Serializable {
+    @Id
+    @Column(name="detalle_actividad_id")
+    private String detalleActividadId;
+    @NotEmpty
+    @Column(name = "nombre_actividad")
+    private String nombreActividad;
+    @NotEmpty
+    @Column(name = "nota_actividad")
+    private int notaActividad;
+    @Column(name = "fecha_creacion")
+    private Date fechaCreacion;
+    @Column(name = "fecha_entrega")
+    private Date fechaEntrega;
+    @Column(name = "fecha_postergacion")
+    private Date fechaPostergacion;
+    @Column(name = "estado")
+    private String estado;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "seminario_id")
+    private Seminario seminario;
+
+}
