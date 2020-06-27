d45a2918b6a31c8f7b0d4ee2f552dfe92d92fccc
Ashly2015 <ranbr200015@gmail.com>
2020-06-26 21:15:35 -0600

    Detalle actividad Dao

---
+++ kalum-notas/src/main/java/edu/kalum/notas/core/models/dao/IDetalleActividadDao.java
@@ -0,0 +1,16 @@
+package edu.kalum.notas.core.models.dao;
+
+import edu.kalum.notas.core.models.entity.DetalleActividad;
+import edu.kalum.notas.core.models.entity.Modulo;
+import edu.kalum.notas.core.models.entity.Seminario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface IDetalleActividadDao extends JpaRepository<DetalleActividad,String> {
+    public List<DetalleActividad> findBySeminario(Seminario seminario);
+
+    @Query("select d from  DetalleActividad s where d.seminario.seminarioId=?1")
+    public List<DetalleActividad> buscarDetalleActividad(String seminarioId);
+}
