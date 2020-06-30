fc10c71631d57abc6acf70eb3300ec1a4551ec57
Ashly2015 <ranbr200015@gmail.com>
2020-06-29 22:28:27 -0600

    Seguimiento de api authentication

--- kalum-oauth/src/main/java/edu/kalum/oauth/core/KalumOauthApplication.java
+++ kalum-oauth/src/main/java/edu/kalum/oauth/core/KalumOauthApplication.java
@@ -1,13 +1,23 @@
 package edu.kalum.oauth.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import sun.tools.jar.CommandLine;
 
 @SpringBootApplication
-public class KalumOauthApplication {
-
+public class KalumOauthApplication implements CommandLineRunner {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     public static void main(String[] args) {
         SpringApplication.run(KalumOauthApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(passwordEncoder.encode("Guatemala"));
+        System.out.println(passwordEncoder.encode("sabado"));
+    }
 }
--- kalum-oauth/src/main/java/edu/kalum/oauth/core/auth/AuthorizationServerConfig.java
+++ kalum-oauth/src/main/java/edu/kalum/oauth/core/auth/AuthorizationServerConfig.java
@@ -2,11 +2,19 @@ package edu.kalum.oauth.core.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -18,6 +26,31 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Qualifier("authenticationManager")
     private AuthenticationManager authenticationManager;
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
 
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("kalum").secret(passwordEncoder.encode("Inicio.2020"))
+                .scopes("read","write").authorizedGrantTypes("password","refresh_token")
+                .accessTokenValiditySeconds(86400).refreshTokenValiditySeconds(86400);
+    }
 
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter());
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return  new JwtTokenStore(accessTokenConverter());
+    }
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter jwtAccessTokenConverter=new JwtAccessTokenConverter();
+        return jwtAccessTokenConverter;
+    }
 }
