bda26d9a8275ac42c6634b90a20bb3676c12bef1
Ashly2015 <ranbr200015@gmail.com>
2020-06-30 22:10:46 -0600

    Diseño de ventana principal

--- TestJPA/TestJPA/src/main/java/org/rachelbarrios/test/controllers/VentanaPrincipalController.java
+++ TestJPA/TestJPA/src/main/java/org/rachelbarrios/test/controllers/VentanaPrincipalController.java
@@ -4,9 +4,12 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.Initializable;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableColumn;
 
 import org.rachelbarrios.test.App;
@@ -15,11 +18,29 @@ import org.rachelbarrios.test.models.Clase;
 import javafx.collections.FXCollections;
 
 public class VentanaPrincipalController implements Initializable {
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private Menu menuC;
+    @FXML
+    private Menu menuR;
+    @FXML
+    private Menu menuH;
+
+
     private App directorEscena;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        anchorPane.styleProperty().set("-fx-background-color: #000000;");
+        menuBar.styleProperty().set("-fx-background-color: #00BFFF; -fx-border-color: #F0FFFF ;");
+        menuC.styleProperty().set(" -fx-border-color: #F0FFFF ;");
+        menuH.styleProperty().set("-fx-border-color: #F0FFFF ;");
+        menuR.styleProperty().set(" -fx-border-color: #F0FFFF ;");
+        
+        
     }
 
     public void mostrarVentanaSalon() {
